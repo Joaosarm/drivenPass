6713d2951740db9570474f4ceeee5683468bf97c
Mateus Brasil <Mateus-code-98>
2022-08-03 09:08:14 -0300

    Preparando revisão de código

--- dist/routers/authRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { validateSchema } from "../middlewares/validateSchema.js";
-import { signUpSchema } from "../schemas/schemas.js";
-import { signIn, signUp } from "../controllers/authController.js";
-var authRouter = Router();
-authRouter.post("/signUp", validateSchema(signUpSchema), signUp);
-authRouter.post("/signIn", validateSchema(signUpSchema), signIn);
-export default authRouter;
